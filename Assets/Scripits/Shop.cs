ba96409906c1783bfc2c8359c33b6e971e07a50a
kostyavrode <inbloomf3@gmail.com>
2024-03-11 22:46:06 +0300

    12312321
    123121

--- Assets/Scripits/Shop.cs
+++ Assets/Scripits/Shop.cs
@@ -6,8 +6,10 @@ public class Shop : MonoBehaviour
 {
     public int cost1;
     public int cost2;
+    public int cost3;
     public GameObject buyButton1;
     public GameObject buyButton2;
+    public GameObject buyButton3;
     private void OnEnable()
     {
         CheckBuy();
@@ -22,12 +24,17 @@ public class Shop : MonoBehaviour
         {
             buyButton2.SetActive(false);
         }
+        if (PlayerPrefs.HasKey("Buy3"))
+        {
+            buyButton2.SetActive(false);
+        }
     }
     public void Buy1()
     {
         if (PlayerPrefs.GetInt("Money")>=cost1)
         {
             PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - cost1);
+            PlayerPrefs.SetString("LevelDone2", "LevelDone2");
             PlayerPrefs.SetString("Buy1","true");
             PlayerPrefs.Save();
             CheckBuy();
@@ -38,7 +45,19 @@ public class Shop : MonoBehaviour
         if (PlayerPrefs.GetInt("Money") >= cost2)
         {
             PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - cost2);
+            PlayerPrefs.SetString("LevelDone3", "LevelDone3");
             PlayerPrefs.SetString("Buy2", "true");
+            PlayerPrefs.Save();
+            CheckBuy();
+        }
+    }
+    public void Buy3()
+    {
+        if (PlayerPrefs.GetInt("Money") >= cost3)
+        {
+            PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - cost3);
+            PlayerPrefs.SetString("LevelDone4", "LevelDone4");
+            PlayerPrefs.SetString("Buy3", "true");
             PlayerPrefs.Save();
             CheckBuy();
         }
