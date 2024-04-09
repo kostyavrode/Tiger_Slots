08ee23630f25e01592c67539e081322a7db597d7
Konstantin <inbloomf3@gmail.com>
2024-04-09 03:34:52 -0700

    12323asZx
    2323423

---
+++ Assets/Purchaser.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+public class Purchaser : MonoBehaviour
+{
+    public void OnPurchaseCompleted(Product product)
+    {
+        switch(product.definition.id)
+        {
+            case "buy.2000coins":
+                AddCoins(2000);
+                break;
+            case "buy.5000coins":
+                AddCoins(5000);
+                break;
+            case "buy.10000coins":
+                AddCoins(10000);
+                break;
+        }
+        Debug.Log(product.definition.id);
+    }
+    private void AddCoins(int count)
+    {
+        PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") + count);
+        PlayerPrefs.Save();
+        UIManager.instance.ShowMoney(PlayerPrefs.GetInt("Money").ToString());
+    }
+}
